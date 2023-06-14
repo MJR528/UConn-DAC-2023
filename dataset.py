dd964567828af44fbad5bdfc8169682ed9919af0
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2023-06-14 03:13:12 -0400

    faster dataset.py

--- dataset.py
+++ dataset.py
@@ -1,4 +1,3 @@
-from decimal import Decimal
 from typing import Optional
 
 import albumentations
@@ -9,9 +8,9 @@ from albumentations import BboxParams
 from albumentations.pytorch import ToTensorV2
 from pylabel.dataset import Dataset as pylabel_dataset
 from torch.utils.data import Dataset
+import numpy as npa
 
-
-class DACDataset(Dataset):
+class DACDataset(Dataset):q
     def __init__(
         self,
         pylabel_ds: pylabel_dataset,
@@ -26,6 +25,7 @@ class DACDataset(Dataset):
         self.split = split
         self.df: pd.DataFrame = self.ds.df[self.ds.df["split"] == self.split]
         self.df.reset_index(inplace=True, drop=True)
+        self.df = self.df.convert_dtypes(infer_objects=True)
         if self.split == "train" and transforms is not None:
             self.transforms = [
                 albumentations.Resize(*resize_to),
@@ -38,7 +38,7 @@ class DACDataset(Dataset):
             self.transforms, BboxParams(format="pascal_voc")
         )
 
-        self.unique_images = self.df.img_filename.unique()
+        self.unique_images = self.df['img_id'].unique()
         self.category_to_int = {
             "Motor Vehicle": 0,
             "Non-motorized Vehicle": 1,
@@ -51,52 +51,73 @@ class DACDataset(Dataset):
             "Dotted lane line": 8,
             "Crosswalk": 9,
         }
+        self.bboxesss = {}
+        self.img_pths = {}
+        for _, row in self.df.iterrows():
+            img_id = row['img_id']
+            
+            if img_id not in self.bboxesss:
+                xmin = float(max(row['ann_bbox_xmin'], 0))
+                ymin = float(max(row['ann_bbox_ymin'], 0))
+                xmax = float(min(row['ann_bbox_xmax'], row['img_width']))
+                ymax = float(min(row['ann_bbox_ymax'], row['img_height']))
+                label_id= int(row['cat_id'])
+                # bboxes = albumentations.core.bbox_utils.convert_bboxes_to_albumentations([xmin, ymin, xmax, ymax, label_id], "pascal_voc",
+            #                                                                          img.shape[:2], check_validity=True)
+                # bboxes = albumentations.core.bbox_utils.convert_bboxes_from_albumentations(b, "yolo", img.shape[:2],
+            #                                                                            check_validity=True)
+                self.bboxesss[img_id] = np.array([[xmin,ymin,xmax,ymax,label_id]])
+                self.img_pths[img_id] = row['img_folder'] + "/" + row['img_filename']
+                continue
+            xmin = float(max(row['ann_bbox_xmin'], 0))
+            ymin = float(max(row['ann_bbox_ymin'], 0))
+            xmax = float(min(row['ann_bbox_xmax'], row['img_width']))
+            ymax = float(min(row['ann_bbox_ymax'], row['img_height']))
+            label_id= int(row['cat_id'])
+            self.bboxesss[img_id] = np.append(self.bboxesss[img_id],[[xmin, ymin, xmax, ymax, label_id]],axis=0)
+        
 
-    def _pascal_voc_to_yolo(self, pascal_voc_lst: list, image_w, image_h):
-        x1, x2, y1, y2, cat = pascal_voc_lst
-        cat_id = self.category_to_int[cat]
-        return [
-            0,
-            cat_id,
-            ((x2 + x1) / (2 * image_w)),
-            ((y2 + y1) / (2 * image_h)),
-            (x2 - x1) / image_w,
-            (y2 - y1) / image_h,
-        ]
+    def _pascal_voc_to_yolo(self, box: list, size):
+        cat = box[-1]
+        # # cat_id = self.category_to_int[cat]
+        # return [
+        #     0,
+        #     cat,
+        #     ((x2 + x1) / (2 * image_w)),
+        #     ((y2 + y1) / (2 * image_h)),
+        #     (x2 - x1) / image_w,
+        #     (y2 - y1) / image_h,
+        # ]
+        dw = 1./(size[0])
+        dh = 1./(size[1])
+        x = (box[0] + box[1])/2.0 - 1
+        y = (box[2] + box[3])/2.0 - 1
+        w = box[1] - box[0]
+        h = box[3] - box[2]
+        x = x*dw
+        w = w*dw
+        y = y*dh
+        h = h*dh
+        
+        return (0,cat,x,y,w,h)
 
     def __len__(self):
         return len(self.unique_images)
 
     def __getitem__(self, idx):
 
-        df = self.df[self.df.img_filename == self.unique_images[idx]]
-        df.reset_index(inplace=True, drop=True)
         # print(df)
-        img_path = df.img_folder[0] + "/" + df.img_filename.iloc[0]
+        img_path = self.img_pths[self.unique_images[idx]]
+        bboxes = self.bboxesss[self.unique_images[idx]]
         image = cv2.imread(img_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        bboxes = []
-        for index, row in df.iterrows():
-            # arranges labels in this format [x_min, y_min, x_max, y_max, label]
-            # Get bounding box coordinates and make sure that they don't extend beyond image boundary
-            xmin = float(Decimal(row.ann_bbox_xmin).max(0))
-            ymin = float(Decimal(row.ann_bbox_ymin).max(0))
-            xmax = float(Decimal(row.ann_bbox_xmax).min(row.img_width))
-            ymax = float(Decimal(row.ann_bbox_ymax).min(row.img_height))
-            label = row.cat_name
-            bboxes.append([xmin, ymin, xmax, ymax, label])
 
-            transformed = self.T(image=image, bboxes=bboxes)
-            img = transformed["image"]
-            bboxes = transformed["bboxes"]
-            # bboxes = albumentations.core.bbox_utils.convert_bboxes_to_albumentations(bboxes, "pascal_voc",
-            #                                                                          img.shape[:2], check_validity=True)
-            # bboxes = albumentations.core.bbox_utils.convert_bboxes_from_albumentations(bboxes, "yolo", img.shape[:2],
-            #                                                                            check_validity=True)
-            # bboxes = np.array(bboxes)
-        return img, torch.tensor(
+        transformed = self.T(image=image, bboxes=bboxes)
+        img = transformed["image"]
+   
+        return img, torch.Tensor(
             [
-                self._pascal_voc_to_yolo(bbox, img.shape[1], img.shape[2])
+                self._pascal_voc_to_yolo(bbox, (img.shape[1], img.shape[2]))
                 for bbox in bboxes
             ]
         )
